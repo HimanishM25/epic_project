fb89c346e1b2ceb08969abbab7224a91f6e07ee4
k_fene_8 <mandrekarh25@gmail.com>
2023-02-27 22:17:54 +0530

    Backend: Setup onboarding screen logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:async';
 
-void main() async{
-  await ScreenUtil.ensureScreenSize();
-  runApp(const MyApp());
-}
+import 'package:after_layout/after_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      color: Colors.blue,
+      home: Splash(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Splash extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  SplashState createState() => SplashState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class SplashState extends State<Splash> with AfterLayoutMixin<Splash> {
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
 
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => Home()));
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) =>  IntroScreen()));
+    }
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) => checkFirstSeen();
 
   @override
   Widget build(BuildContext context) {
-return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-              color: Colors.transparent, toolbarTextStyle: TextStyle()),
-          fontFamily: 'Inter',
-        ),
-        home: ScreenUtilInit(
-            designSize: const Size(414, 896), builder: () => firstpage()));
+    return Scaffold(
+      body: Center(
+        child: Text('Loading...'),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: Center(
+        child: Text('This is the second page'),
+      ),
+    );
+  }
+}
+
+class IntroScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IntroScreen'),
+      ),
+      body: Center(
+        child: Text('This is the IntroScreen'),
+      ),
+    );
   }
 }
