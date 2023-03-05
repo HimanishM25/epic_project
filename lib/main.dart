0d7fdd438ea1b9e2d727f2df41be5f997f5120a4
k_fene_8 <mandrekarh25@gmail.com>
2023-03-05 20:17:39 +0530

    Frontend: Set Inter as default app font.

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-              theme: ThemeData(
-          brightness: Brightness.light,
-          drawerTheme: DrawerThemeData(backgroundColor: Colors.grey.shade200),
-          scaffoldBackgroundColor: Colors.white,
-          primaryColor: Color(0xff00C880),
-          cardColor: Colors.grey.shade200,
-          appBarTheme: const AppBarTheme(
-            iconTheme: IconThemeData(color: Colors.black),
-            color: Colors.transparent,
-            foregroundColor: Colors.black,
-          ),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        fontFamily: 'Inter',
+        drawerTheme: DrawerThemeData(backgroundColor: Colors.grey.shade200),
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: Color(0xff00C880),
+        cardColor: Colors.grey.shade200,
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.black),
+          color: Colors.transparent,
+          foregroundColor: Colors.black,
         ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          drawerTheme: DrawerThemeData(backgroundColor: Colors.black),
-          scaffoldBackgroundColor: Colors.black,
-          primaryColor: Color(0xff00C880),
-          cardColor: Colors.grey.shade800,
-          appBarTheme: AppBarTheme(
-            color: Colors.transparent,
-            foregroundColor: Colors.white,
-          ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Inter',
+        drawerTheme: DrawerThemeData(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: Colors.black,
+        primaryColor: Color(0xff00C880),
+        cardColor: Colors.grey.shade800,
+        appBarTheme: AppBarTheme(
+          color: Colors.transparent,
+          foregroundColor: Colors.white,
         ),
-        themeMode: ThemeMode.system,
-        title: 'Ceres',
-        
-        home: const navSetup(),
+      ),
+      themeMode: ThemeMode.system,
+      title: 'Ceres',
+      home: const navSetup(),
     );
   }
 }
-
