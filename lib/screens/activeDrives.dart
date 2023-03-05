466a6aab14c234e4c39635a491d9d56affd426cd
k_fene_8 <mandrekarh25@gmail.com>
2023-03-05 15:00:43 +0530

    Frontend: Setup display pages for user.

---
+++ lib/screens/activeDrives.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class activeDrives extends StatefulWidget {
+  const activeDrives({super.key});
+
+  @override
+  State<activeDrives> createState() => _activeDrivesState();
+}
+
+class _activeDrivesState extends State<activeDrives> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
