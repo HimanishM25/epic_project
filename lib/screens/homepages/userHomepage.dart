e972b2e39e617da10237a8ff4eeecf7f22450f2e
k_fene_8 <mandrekarh25@gmail.com>
2023-05-19 09:02:49 +0530

    Frontend:Match UI to prototype.

--- lib/screens/homepages/userHomepage.dart
+++ lib/screens/homepages/userHomepage.dart
@@ -18,148 +18,192 @@ class userHomepage extends StatefulWidget {
 class _userHomepageState extends State<userHomepage> {
   CarouselController buttonCarouselController = CarouselController();
   var database = FirebaseFirestore.instance.collection('drives').snapshots();
-                
+
   final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+    'https://img.freepik.com/free-photo/plate-food-with-different-dishes-including-chicken-rice-other-food_1340-24267.jpg?w=1800&t=st=1683993234~exp=1683993834~hmac=1a1fd21a175eef8da1d52906a1554098e59cdfd8ca6d95c85aa0a804747995f1',
+    'https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg?w=400&h=300&c=crop',
+    'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/1640774/pexels-photo-1640774.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/718742/pexels-photo-718742.jpeg?auto=compress&cs=tinysrgb&w=800',
+    'https://images.pexels.com/photos/406152/pexels-photo-406152.jpeg?auto=compress&cs=tinysrgb&w=800'
   ];
+
   @override
   Widget build(BuildContext context) => Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text('Make Quick Donations',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  )),
-              const SizedBox(height: 20),
-              CarouselSlider(
-                items: imgList
-                    .map(
-                      (item) =>
-                          Stack(alignment: Alignment.bottomCenter, children: [
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: Image.network(item, fit: BoxFit.cover)),
-                        const Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
-                          child: Text(
-                            '50% funded',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text('Make Quick Donations',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const SizedBox(height: 20),
+                CarouselSlider(
+                  items: imgList
+                      .map(
+                        (item) =>
+                            Stack(alignment: Alignment.bottomCenter, children: [
+                          ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: Image.network(item, fit: BoxFit.cover)),
+                          const Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                            child: Text(
+                              '50% funded',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: LinearProgressIndicator(
-                            value: 0.5,
-                            backgroundColor: Colors.white,
-                            valueColor:
-                                AlwaysStoppedAnimation<Color>(Colors.green),
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: LinearProgressIndicator(
+                              value: 0.5,
+                              backgroundColor: Colors.white,
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.green),
+                            ),
+                          ),
+                        ]),
+                      )
+                      .toList(),
+                  carouselController: buttonCarouselController,
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    pauseAutoPlayOnTouch: true,
+                    pauseAutoPlayInFiniteScroll: true,
+                    enlargeCenterPage: true,
+                    padEnds: true,
+                    viewportFraction: 0.7,
+                    aspectRatio: 2.0,
+                    initialPage: 1,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 60,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFF00C880),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => activeDrives()),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text('View active campaigns',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          const Icon(Icons.arrow_forward)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 60,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFF00C880),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => DonatePage()),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text('Volunteer',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          const Icon(Icons.arrow_forward)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text('Your Stats',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00C880),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Donated 200!",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20,
                           ),
                         ),
-                      ]),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00C880),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Volunteered \n 5 times!',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     )
-                    .toList(),
-                carouselController: buttonCarouselController,
-                options: CarouselOptions(
-                  autoPlay: true,
-                  pauseAutoPlayOnTouch: true,
-                  pauseAutoPlayInFiniteScroll: true,
-                  enlargeCenterPage: true,
-                  padEnds: true,
-                  viewportFraction: 0.7,
-                  aspectRatio: 2.0,
-                  initialPage: 1,
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 60,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: const Color(0xFF00C880),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => activeDrives()),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('View active campaigns',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        const Icon(Icons.arrow_forward)
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 60,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: const Color(0xFF00C880),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => DonatePage()),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('Volunteer',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        const Icon(Icons.arrow_forward)
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text('Your Stats',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  )),
-              const SizedBox(height: 20),
-            ]),
+                  ],
+                )
+              ]),
+        ),
       );
 }
